06a1be07adb9872db48b55f99f65b114fe19e54f
calhank <hank.m@berkeley.edu>
2015-10-28 18:31:55 -0400

    can count hashtags?

--- twitter_popularity.scala
+++ twitter_popularity.scala
@@ -49,9 +49,11 @@ object Main extends App {
     // 		rdd.take(top).foreach{ case (tag, user, ats) => println("%s by %s at %s".format(tag, user, ats.mkString(", ") )) }
     // 	})
 
-    val topHashtags = hashnum.reduceByKeyAndWindow(_ + _ , Seconds(6)).map{case(hash, num) => (num, hash)}.transform(_.sortByKey(false))
+    val topHashtags = hashnum.reduceByKeyAndWindow(_ + _ , Seconds(6), Seconds(6)).map{case(hash, num) => (num, hash)}.transform(_.sortByKey(false))
 
-    topHashtags.print()
+    val joinstuff = topHashtags.join(hashfirst)
+
+    joinstuff.print()
 
     // statuses.saveAsTextFiles("http://50.23.16.227:19998/statuses")
 
