bd4fd8adf8d9c32c3cf1b386d5b66629be9f0c61
calhank <hank.m@berkeley.edu>
2015-10-27 21:04:32 -0400

    dunno what im doin

--- twitter_popularity.scala
+++ twitter_popularity.scala
@@ -7,8 +7,8 @@ object Main extends App {
 	println(s"I got executed with ${args size} args, they are: ${args mkString ", "}")
 
 	// your code goes here
-	val ssc = new StreamingContext(conf, Seconds(2))
 	val sparkConf = new SparkConf().setAppName("twitter_popularity")
+	val ssc = new StreamingContext(sparkConf, Seconds(2))
 	val stream = TwitterUtils.createStream(ssc, None)
 	val users = stream.map(status => status.getUser.getId)
 
