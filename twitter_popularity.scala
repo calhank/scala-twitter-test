448d814e9bb98b2067b96f190c2d760e5f42a456
calhank <hank.m@berkeley.edu>
2015-11-02 18:32:37 -0500

    duupe

--- twitter_popularity.scala
+++ twitter_popularity.scala
@@ -56,8 +56,8 @@ object Main extends App {
 	val aggregatedHashtags = hashfirst.window(Seconds(runtime), Seconds(window)).combineByKey( 
 		(users: Set[String]) => (users, 1),
 		(combiner: (Set[String], Int), users: Set[String]) => ( combiner._1 ++ users, combiner._2 + 1 ),
-		(comb1: (Set[String], Int), comb2: (Set[String], Int)) => (comb1._1 ++ comb2._1, comb1._2 + comb2._2)
-	 )
+		(comb1: (Set[String], Int), comb2: (Set[String], Int)) => (comb1._1 ++ comb2._1, comb1._2 + comb2._2),
+		new org.apache.spark.HashPartitioner(10/2))
 	.map{ case(tag, (users, count)) => (count, (tag, users))}
 	.transform(_.sortByKey(false))
 
