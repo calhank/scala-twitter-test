ce5372304ca67ef00a26c10a1fa041cb99390149
calhank <hank.m@berkeley.edu>
2015-11-01 19:50:30 -0500

    duupe

--- twitter_popularity.scala
+++ twitter_popularity.scala
@@ -55,7 +55,7 @@ object Main extends App {
 
 	val aggregatedHashtags = hashfirst.reduceByKey( 
 		(users: Set[String]) => (users, 1),
-		(combiner: (Set[String], Int), users: set[String]) => ( combiner._1 ++ users, combiner._2 + 1 ),
+		(combiner: (Set[String], Int), users: Set[String]) => ( combiner._1 ++ users, combiner._2 + 1 ),
 		(comb1: (Set[String], Int), comb2: (Set[String], Int)) => (comb1._1 ++ comb2._1, comb1._2 ++ comb2._2)
 	 )
 	.map{ case(tag, (users, count)) => (count, (tag, users))}
