e7e12adae39ffe7d8d1382efb40acf56c1704adf
calhank <hank.m@berkeley.edu>
2015-11-02 22:49:35 -0500

    autocheckin

--- twitter_popularity.scala
+++ twitter_popularity.scala
@@ -18,18 +18,15 @@ object Main extends App {
     val top = args(2).toInt
     val filters = args.takeRight( args.length - 3 )
 
-	println("\nTwitter Hashtag Streaming\nBatch Length: %s seconds\nRuntime : %s seconds\nTop Tweets: %s\nFilters: %s\n".format(window, runtime, top, filters.mkString(", ")))
+	println("\nTwitter Hashtag Streaming\nBatch Length: %s seconds\nRuntime : %s seconds\nNumber of Top Tweets: %s\nFilters: %s\n".format(window, runtime, top, filters.mkString(", ")))
 
-    // Set the system properties so that Twitter4j library used by twitter stream
-    // can use them to generat OAuth credentials
     System.setProperty("twitter4j.oauth.consumerKey", "U2UbUq8DX2WlbfbR9m3nY0tcW")
     System.setProperty("twitter4j.oauth.consumerSecret", "m4NHVi738gGyzD4pNLP8lxuoGOlV4TqUwKL0MHqtm1sGwkB9lV")
     System.setProperty("twitter4j.oauth.accessToken", "286789101-m9DbIXjfU5zeddtIsMRtgQ5DeNy64hnDOVr2KFnB")
     System.setProperty("twitter4j.oauth.accessTokenSecret", "H8DSy6MrLmMNnqk9IJh4JiTuk0XsDAmTfNgwmcb9OuQvk")
 
     val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
-    val ssc = new StreamingContext(sparkConf, Seconds( 10 ))
-    // val ssc = new StreamingContext(sparkConf, Seconds( 2 ) )
+    val ssc = new StreamingContext(sparkConf, Seconds( window ))
     val stream = TwitterUtils.createStream(ssc, None, filters)
 
     val tweets = stream.map(status => ( status.getUser().getScreenName(), status.getText().split(" ") ) )
@@ -39,7 +36,6 @@ object Main extends App {
     val parsedTweetsWithHash = parsedTweets.filter{ case (_, hashtags, _) => hashtags.length > 0 }
 
 	val hashfirst = parsedTweetsWithHash.flatMap{ case(user, hashtags, ats) => hashtags.map( tag => ( tag, user + " " + ats.mkString(" ") + " ") )  }
-
 	
 	hashfirst.persist(StorageLevel.OFF_HEAP)
 
@@ -53,7 +49,7 @@ object Main extends App {
 
 	aggregatedHashtags.foreachRDD( rdd => {
 		println("\nTop Results:")
-		rdd.collect().take(top).foreach{ case ( num, (tag, users) ) => println("%s tweeted %s times with users: %s".format( tag, num, users ) ) }
+		rdd.collect().take(top).foreach{ case ( num, (tag, users) ) => println("::: %s was tweeted %s times with users: %s".format( tag, num, users ) ) }
 		})
 
 	ssc.start()
